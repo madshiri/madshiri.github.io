43b594d5798d68b01ddc7b6228e87a73742c2be2
madshiri <mohammadshiri.m@gmail.com>
2022-09-05 12:11:57 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Academic Website
+
+This is my personal academic website. (https://madshiri.github.io)
---
+++ config.js
@@ -0,0 +1,4 @@
+window.__PAW_CONFIG__ = {
+    googleApiKey: 'AIzaSyDJb3vUY3ekGBpKPmGwPQHF4wSAftNfGEY',
+    googleSheetDocId: '1c5MbmX5dOFe0KQWGe1VZVDYldfYvH3gbpWSQGQtv_DA',
+}
